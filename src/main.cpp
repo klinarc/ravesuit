1ad5f91647fd2521d8c0dff27ea6f8960c674cfb
Markus <atiemark1@aon.at>
2019-12-17 14:21:31 +0100

    stretch sensor v1

---
+++ src/main.cpp
@@ -0,0 +1,147 @@
+/*
+ *  This sketch demonstrates how to scan WiFi networks.
+ *  The API is almost the same as with the WiFi Shield library,
+ *  the most obvious difference being the different file you need to include:
+ */
+#include "WiFi.h"
+#include <ArduinoOSC.h>
+OscWiFi osc;
+
+const char* host = "192.168.43.114";
+const int recv_port = 10000;
+const int send_port = 12000;
+
+const char* ssid = "HUAWEI nova 2";
+const char* password =  "kunstuni";
+
+
+//Analog Input
+#define ANALOG_PIN_0 36
+int analog_value = 0;
+
+void onOscReceived(OscMessage& m)
+{
+    Serial.print("callback : ");
+    Serial.print(m.ip()); Serial.print(" ");
+    Serial.print(m.port()); Serial.print(" ");
+    Serial.print(m.size()); Serial.print(" ");
+    Serial.print(m.address()); Serial.print(" ");
+    Serial.print(m.arg<int>(0)); Serial.print(" ");
+    Serial.print(m.arg<float>(1)); Serial.print(" ");
+    Serial.print(m.arg<String>(2)); Serial.println();
+}
+
+void wifiConnect(){
+    WiFi.begin(ssid, password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print("Connecting to WiFi ");
+    }
+    
+   Serial.print("WiFi connected, IP = "); Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    wifiConnect();
+
+    Serial.println("Setup done");
+
+    // ArduinoOSC
+    osc.begin(recv_port);
+
+    // TODO: TBD
+    // osc.subscribe("/int32", i);
+    // osc.subscribe("/float", f);
+    // osc.subscribe("/string", s);
+    // osc.subscribe("/blob", b);
+
+    osc.subscribe("/callback", onOscReceived); // old style (v0.1.x)
+
+    osc.subscribe("/lambda", [](OscMessage& m)
+    {
+        Serial.print("lambda : ");
+        Serial.print(m.ip()); Serial.print(" ");
+        Serial.print(m.port()); Serial.print(" ");
+        Serial.print(m.size()); Serial.print(" ");
+        Serial.print(m.address()); Serial.print(" ");
+        Serial.print(m.arg<int>(0)); Serial.print(" ");
+        Serial.print(m.arg<float>(1)); Serial.print(" ");
+        Serial.print(m.arg<String>(2)); Serial.println();
+    });
+    osc.subscribe("/wildcard/*/test", [](OscMessage& m)
+    {
+        Serial.print("wildcard : ");
+        Serial.print(m.ip()); Serial.print(" ");
+        Serial.print(m.port()); Serial.print(" ");
+        Serial.print(m.size()); Serial.print(" ");
+        Serial.print(m.address()); Serial.print(" ");
+        Serial.print(m.arg<int>(0)); Serial.println();
+
+    });
+    osc.subscribe("/need/reply", [](OscMessage& m)
+    {
+        Serial.println("/need/reply");
+
+        int i = 12;
+        float f = 34.56F;
+        double d = 78.987;
+        String s = "hello";
+        bool b = true;
+
+        osc.send(host, send_port, "/send", i, f, d, s, b);
+    });
+
+    // TODO: TBD
+    // osc.publish(host, send_port, "/value", value);
+    // osc.publish(host, send_port, "/millis", &millis);
+}
+
+
+
+void wifiScan() {
+Serial.println("scan start");
+
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            delay(10);
+        }
+    }
+    Serial.println("");
+
+    // Wait a bit before scanning again
+    delay(5000);
+}
+
+
+void loop()
+{
+     //osc.parse(); // should be called
+
+     analog_value = analogRead(ANALOG_PIN_0);
+    Serial.println(analog_value);
+    osc.send(host, send_port, "/Analog", analog_value);
+}
